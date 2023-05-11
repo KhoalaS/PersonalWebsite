5466e85c1d900b11ca76a6273c497c459946585f
KhoalaS <83372697+KhoalaS@users.noreply.github.com>
2023-05-11 21:37:20 +0000

    Deploying to gh-pages from @ KhoalaS/PersonalWebsite@e59c248eb752d195e79f72fdc797dd3a8a7ec275 🚀

--- src/Keys.ts
+++
@@ -1,5 +0,0 @@
-import type { OutputLine } from './Types'
-import type { InjectionKey, Ref } from 'vue'
-
-export const shellOutputKey = Symbol() as InjectionKey<Ref<Array<OutputLine>>>
-export const shellInputKey = Symbol() as InjectionKey<Ref<string>>
