5d4c031a9b1c5f09745ee4839632d5722545124e
KhoalaS <83372697+KhoalaS@users.noreply.github.com>
2023-05-11 21:34:12 +0000

    Deploying to gh-pages from @ KhoalaS/PersonalWebsite@7b58a4bb867402e63736c1bfabf6b136a544e6fa 🚀

---
+++ src/components/CatArea.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import catgif from '@/assets/cat_kiss.gif'
+import amongusTwerk from '@/assets/among_us_twerk.gif'
+</script>
+
+<template>
+  <main class="flex">
+    <img :src="catgif" />
+    <img :src="amongusTwerk" />
+  </main>
+</template>
